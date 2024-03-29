32bbacde46128f67ab5a30fdaf4ac73a87ada517
Artem <artemuhov@gmail.com>
2024-03-29 11:06:52 +0100

    first commit

---
+++ db-seed.py
@@ -0,0 +1,35 @@
+# Import Flask modules
+from flask import Flask, request, render_template
+from flaskext.mysql import MySQL
+
+# Create an object named app
+app = Flask(__name__)
+
+# This "/home/ec2-user/dbserver.endpoint" file has to be created from cloudformation
+# template and it has RDS endpoint
+db_endpoint = open("/home/ec2-user/dbserver.endpoint", "r", encoding="UTF-8")
+
+# Configure mysql database
+app.config["MYSQL_DATABASE_HOST"] = db_endpoint.readline().strip()
+app.config["MYSQL_DATABASE_USER"] = "admin"
+app.config["MYSQL_DATABASE_PASSWORD"] = "clarusway_1234"
+app.config["MYSQL_DATABASE_DB"] = "clarusway_phonebook"
+app.config["MYSQL_DATABASE_PORT"] = 3306
+db_endpoint.close()
+mysql = MySQL()
+mysql.init_app(app)
+connection = mysql.connect()
+connection.autocommit(True)
+cursor = connection.cursor()
+
+
+def read_file_into_string(file_path):
+    with open(file_path, "r", encoding="UTF-8") as file:
+        file_content = file.read()
+    return file_content
+
+
+data_phonebook_table = read_file_into_string("/home/ec2-user/app/phonebook.sql")
+# print(data_phonebook_table)
+cursor.execute(data_phonebook_table)
+print("Data imported successfully!")
